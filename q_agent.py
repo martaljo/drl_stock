fbf206ec28b02505f672c0dda5a2919a675a7127
jorgemartal <martalba.jorge@gmail.com>
2023-06-06 23:34:54 -0700

    First working agent, positive rewards

--- q_agent.py
+++ q_agent.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 BUY = 0
 SELL = 1
+SELL_ALL = -1
 
 class QLearningAgent:
-    def __init__(self, action_size, learning_rate=0.1, discount_factor=0.9, exploration_rate=1.0, exploration_decay_rate=0.001):
+    def __init__(self, action_size, learning_rate=0.1, discount_factor=0.9, exploration_rate=1.0, exploration_decay_rate=0.0001):
         self.action_size = action_size
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
@@ -58,34 +59,78 @@ class TradingEnvironment:
         return tuple(self.data[self.current_step])
         # return (self.capital, self.inventory)
 
+    def get_stock_value(self):
+        return self.inventory["AAPL"][0] * self.inventory["AAPL"][1]
+
+    def sell_stocks(self, ticker, quantity, current_price):
+        curr_inventory = self.inventory[ticker]
+        sell_amount = curr_inventory[1] * quantity
+        
+        self.inventory[ticker] = (curr_inventory - quantity, curr_inventory[1])
+        self.capital += sell_amount
+
+        return sell_amount
+
+    def buy_stocks(self, ticker, quantity, current_price):
+        curr_ticker_inv = self.inventory[ticker]
+        current_amount = curr_ticker_inv[0] * curr_ticker_inv[1]
+        buy_amount = current_price * quantity
+
+        new_quantity = curr_ticker_inv[0] + quantity
+        new_mean_price = (current_amount + buy_amount) / new_quantity
+
+        self.inventory[ticker] = (new_quantity, new_mean_price)
+        self.capital -= buy_amount
+
+        return buy_amount
+
     def take_action(self, action, current_price, next_price):
         reward = 0
+        hold = False
         if action == BUY:  # Buy
             if self.capital > current_price:
-                # self.inventory += 1
-                self.capital -= current_price
-                current = self.inventory["AAPL"]
-                self.inventory["AAPL"] = (current[0] + 1, (current[1] * current[0] + current_price)/(current[0] +1))
+                self.buy_stocks("AAPL", 1, current_price)
+            else:
+                hold=True
         elif action == SELL:  # Sell
             if self.inventory["AAPL"][0] > 0:
-                reward = (next_price - self.inventory["AAPL"][1]) * self.inventory["AAPL"][0]
-                self.capital += self.inventory["AAPL"][0] * next_price
-                self.inventory["AAPL"] = (0, 0.0)
-                # self.inventory = 0
+                current = self.inventory["AAPL"]
+                reward = (next_price - current[1])
+                self.capital += next_price
+                self.inventory["AAPL"] = (current[0] - 1, current[1])
+
+                # self.sell_stocks("AAPL", 1, current_price)
+            else:
+                hold = True
+        elif action == SELL_ALL:
+            current = self.inventory["AAPL"]
+            reward = (next_price - current[1]) * current[0]
+            self.capital += current[0] * next_price
+            self.inventory["AAPL"] = (0, 0.0)
+            
+            # self.sell_stocks("AAPL", current[0], current_price)
         else:  # Hold
             if self.inventory["AAPL"][0] > 0:
-                reward = (next_price - self.inventory["AAPL"][1]) * self.inventory["AAPL"][0]
+                reward = (current_price - self.inventory["AAPL"][1]) * self.inventory["AAPL"][0]
+
+        if hold:
+            if self.inventory["AAPL"][0] > 0:
+                reward = (current_price - self.inventory["AAPL"][1]) * self.inventory["AAPL"][0]
         return reward
 
     def step(self, action):
-        current_price = self.data[self.current_step][2]
+        current_price = self.data[-self.current_step][0]
         self.current_step += 1
         done = self.current_step >= self.total_steps
         if done:
             next_state = tuple(self.data[self.current_step - 1])
             reward = 0
+        elif self.current_step == self.total_steps -1:
+            next_price = self.data[-self.current_step - 1][2]
+            reward = self.take_action(SELL_ALL, current_price, next_price)
+            next_state = self.get_state()
         else:
-            next_price = self.data[self.current_step][0]
+            next_price = self.data[-self.current_step - 1][2]
             reward = self.take_action(action, current_price, next_price)
             next_state = self.get_state()
         return next_state, reward, done
@@ -101,7 +146,7 @@ action_size = 3
 learning_rate = 0.1
 discount_factor = 0.9
 exploration_rate = 1.0
-total_episodes = 2000
+total_episodes = 10000
 
 env = TradingEnvironment(data)
 agent = QLearningAgent(action_size, learning_rate, discount_factor, exploration_rate)
@@ -119,13 +164,16 @@ for episode in range(total_episodes):
         agent.update_q_table(state, action, reward, next_state)
         total_reward += reward
         state = next_state
+        if done:
+            total_reward += env.get_stock_value()
 
     agent.decay_exploration_rate()
     total_rewards.append(total_reward)
 
     if episode % 100 == 0:
         print(f"Episode: {episode}, Total Reward: {total_reward}, Exploration Rate: {agent.exploration_rate}")
-    # print(action)
+
+print(f"Finished running with {env.capital + env.get_stock_value()}")
 
 plt.plot(total_rewards)
 plt.xlabel('Episode')
